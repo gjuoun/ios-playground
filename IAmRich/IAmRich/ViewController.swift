94ec51068bf37102b21d614ecf5357b39bf2bb7d
Jun Guo <gjuoun@gmail.com>
2022-09-15 00:29:26 -0300

    im rich

---
+++ IAmRich/IAmRich/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  IAmRich
+//
+//  Created by Jun Guo on 2022-09-14.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
