e6b0f9ae55e1b2fd62fbb03ee1161e647c063db4
Jun Guo <gjuoun@gmail.com>
2022-09-15 05:19:31 -0300

    done calculator layout

--- IAmPoor/IAmPoor/ViewController.swift
+++ IAmPoor/IAmPoor/ViewController.swift
@@ -9,11 +9,27 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    
+    @IBOutlet weak var imageButton: UIButton!
+    var imageName: String = "emerald"
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
 
-
+    
+    
+    @IBAction func onClick(_ sender: UIButton) {
+        if imageName == "emerald" {
+            imageName = "sapphire"
+        }else{
+            imageName = "emerald"
+        }
+            
+        imageButton.setBackgroundImage(UIImage(imageLiteralResourceName: imageName), for: .normal)
+    
+    }
+    
 }
 
