778c853670a2cc9077abdd41dccf6364281f87cb
Jun Guo <gjuoun@gmail.com>
2022-09-27 01:15:02 -0300

    update

---
+++ UrlSchemeTest/UrlSchemeTest/UrlSchemeTestApp.swift
@@ -0,0 +1,17 @@
+//
+//  UrlSchemeTestApp.swift
+//  UrlSchemeTest
+//
+//  Created by Jun Guo on 2022-09-21.
+//
+
+import SwiftUI
+
+@main
+struct UrlSchemeTestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ app2/app2/app2App.swift
@@ -0,0 +1,17 @@
+//
+//  app2App.swift
+//  app2
+//
+//  Created by Jun Guo on 2022-09-22.
+//
+
+import SwiftUI
+
+@main
+struct app2App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
